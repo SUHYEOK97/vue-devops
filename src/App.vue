cd42fb9368d9e2c32565a5a8a792aa2f723d829b
SUHYEOK97 <rlatngur10@naver.com>
2021-06-29 15:46:27 +0900

    Add deploy workflow

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to 김수혁 Vue.js App" />
 </template>
 
 <script>
